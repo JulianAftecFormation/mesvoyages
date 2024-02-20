1b53f8acc4245a741fb797049f4d24dd19312927
Julian <julian.vieira-gebcyznski@etu.formation-aftec.com>
2024-02-20 15:13:59 +0100

    Phase 1 : création du projet

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Controller;
+
+/**
+ * Description of AccueilController
+ *
+ * @author Toochi
+ */
+class AccueilController {
+    //put your code here
+}
