f96a22f96db2aedec17dbcf0e0dec3885ab58231
Julian <julian.vieira-gebcyznski@etu.formation-aftec.com>
2024-03-12 08:52:25 +0100

    Phase 1.1 : creation hello world

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -7,11 +7,22 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 /**
- * Description of AccueilController
+ * Description of AccueilControllers
  *
  * @author Toochi
  */
 class AccueilController {
+    
+    /**
+     * @Route("/", name="accueil")
+     * @return Response
+     */
+    
+    public function index(): Response{
+        return new Response("Hello World !");
+    }
     //put your code here
 }
