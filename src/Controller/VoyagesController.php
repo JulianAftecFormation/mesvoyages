262be7de3265dddfebf1fdd18a337a0db552bae1
Julian <julian.vieira-gebcyznski@etu.formation-aftec.com>
2024-03-13 10:03:57 +0100

    Phase 1.5 : Page d'accueil, et bug réglé

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -11,14 +11,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 /**
- * Description of AccueilControllers
+ * Description of VoyagesController
  *
  * @author Toochi
  */
-class AccueilController extends AbstractController{
-    
-    public function index(): Response{
-        return $this->render("pages\accueil.html.twig");
+class VoyagesController extends AbstractController{
+     
+    /**
+     * @Route("/voyages", name="voyages")
+     * @return Response
+     */
+    public function voyages(): Response{
+        return $this->render("pages/voyages.html.twig");
     }
     //put your code here
 }
