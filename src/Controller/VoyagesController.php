205e5b1ce9bdeb74635c957e858eb7d3d8b42382
Julian <julian.vieira-gebcyznski@etu.formation-aftec.com>
2024-03-21 14:08:33 +0100

    Phase 11 : ORM (nouvelle page pour l'affichage détaillé d'un enregistrement).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -72,5 +72,18 @@ class VoyagesController extends AbstractController{
             'visites' => $visites
         ]);
     }
+    
+    /**
+     * @Route("/voyages/voyage/{id}", name="voyages.showone")
+     * @param type $id
+     * @return Response
+     */
+    public function showOne($id): Response {
+        $visite = $this->repository->find($id);
+        return $this->render("pages/voyage.html.twig",[
+            'visite' => $visite
+        ]);
+            
+    }
   
 }
