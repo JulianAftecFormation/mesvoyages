f1a2993cb6be188dbe3c94eeb0d9b0cf478dc279
Julian <julian.vieira-gebcyznski@etu.formation-aftec.com>
2024-04-01 14:58:07 +0200

    Phase 22 : codage de la page d'accueil du site.

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -7,6 +7,7 @@
 
 namespace App\Controller;
 
+use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,13 +17,30 @@ use Symfony\Component\Routing\Annotation\Route;
  * @author Toochi
  */
 class AccueilController extends AbstractController{
+    
+    /**
+     *
+     * @var VisiteRepository
+     */
+    private $repository;
+
+    /**
+     * 
+     * @param VisiteRepository $repository
+     */
+    public function __construct(VisiteRepository $repository) {
+        $this->repository = $repository;
+    }
 
     /**
      * @Route("/", name="accueil")
      * @return Response
      */
     public function index(): Response{
-        return $this->render("pages\accueil.html.twig");
+        $visites = $this->repository->findAllLasted(2);
+        return $this->render("pages/accueil.html.twig", [
+            'visites' => $visites
+        ]); 
     }
     //put your code here
 }
--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -70,4 +70,17 @@ class VisiteRepository extends ServiceEntityRepository
                     ->getResult();                   
         }
     }
+    
+    /**
+     * Retourne les n visites les plus récentes
+     * @param type $nb
+     * @return Visite[]
+     */
+    public function findAllLasted($nb) : array {
+        return $this->createQueryBuilder('v') // alias de la table
+           ->orderBy('v.datecreation', 'DESC')
+           ->setMaxResults($nb)     
+           ->getQuery()
+           ->getResult();
+    }
 }
